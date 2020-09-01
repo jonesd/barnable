e847393582a8b42fe8c8d50f1fe3269478bd07fe
David Jones <david@dgjones.info>
2020-09-01 10:06:54 -0400

    Skip jacocoa as it is interferring with debugging tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "1.4.0"
-    jacoco
+//    jacoco
 }
 
 group = "info.dgjones.au"
@@ -29,10 +29,10 @@ tasks.withType<Test> {
     useJUnitPlatform()
 }
 
-tasks.test {
-    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
-}
-tasks.jacocoTestReport {
-    dependsOn(tasks.test) // tests are required to run before generating the report
-}
+//tasks.test {
+//    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+//}
+//tasks.jacocoTestReport {
+//    dependsOn(tasks.test) // tests are required to run before generating the report
+//}
 
